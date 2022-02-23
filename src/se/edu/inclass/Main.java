5b4634efcca99eb01af3a9edd22379fd459bb1c4
FaliciaOng <ongfali@gmail.com>
2022-02-23 17:04:45 +0800

    Use count with stream in main

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -20,6 +20,8 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        System.out.println("Total number of deadlines (using Stream) : " +
+                countDeadlinesWithStreams(tasksData));
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
